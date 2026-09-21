59a50d5dd0b69ebf37929bdee9d92f2f354bfe63
Gabriel Giglio <giglio.gabriel1@yahoo.com.br>
2026-09-20 20:04:02 -0600

    fix: ajuste do fluxo de inicializacao para o login e configuracao da stack raiz

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Redirect } from 'expo-router';
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Instagram Clone - Setup Inicial</Text>
-    </View>
-  );
+export default function Index() {
+  return <Redirect href={'/login' as any} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
